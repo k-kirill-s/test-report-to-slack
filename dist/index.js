e9585f801ba81feb3f142240ca038ff909d17a1f
Kirill Kaluga <kirill.kaluga.s@gmail.com>
2024-02-22 14:32:31 +0300

    add markdown and url features

--- dist/index.js
+++ dist/index.js
@@ -38003,7 +38003,7 @@ function parseTestResults(jsonPath) {
     const total = stats.expected || 0;
     const passed = total - (stats.unexpected || 0);
     const failed = stats.unexpected || 0;
-    const duration = stats.duration || 0;
+    const duration = Math.ceil(stats.duration / 1000) || 0;
 
     return {
         total,
@@ -38013,24 +38013,35 @@ function parseTestResults(jsonPath) {
     };
 }
 
+function getStatusEmoji(passed, total) {
+    const ratio = passed / total;
+    if (ratio === 1) return ':heavy_check_mark:'; 
+    if (ratio > 0) return ':warning:';
+    return ':x:';
+}
+
 async function run() {
     try {
         const title = getInput('title');
         const slackBotToken = getInput('slack-bot-token');
         const slackChannel = getInput('slack-channel');
         const jsonPath = getInput('json-path');
-        const url = getInput('url', { required: false });
+
+        const githubRepository = process.env.GITHUB_REPOSITORY;
+        const githubRunId = process.env.GITHUB_RUN_ID;
+        url = `https://github.com/${githubRepository}/actions/runs/${githubRunId}`;
 
         const { total, passed, failed, duration } = parseTestResults(jsonPath);
-        let messageText = title + ` | Test Results: Total: ${total}, Passed: ${passed}, Failed: ${failed}, Duration: ${duration}ms`;
-        if (url) {
-            messageText += ` | Details: ${url}`;
-        }
+        const statusEmoji = getStatusEmoji(passed, total);
+        let messageText = `${statusEmoji} *<${url}|${title}>*\n`;
+        messageText += `Total: ${total}, Passed: ${passed}, Failed: ${failed}, Duration: ${duration}s\n`;
+
 
         const slack = new WebClient(slackBotToken);
         await slack.chat.postMessage({
             channel: slackChannel,
             text: messageText,
+            mrkdwn: true,
         });
 
         console.log("Message sent to Slack successfully.");
